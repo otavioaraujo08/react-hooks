33178e185b233e37c929facb84c7f0ed7287b871
otavioaraujo08 <otavioaraujo490@gmail.com>
2022-07-05 17:32:18 -0300

    feat: 'feat: UseReducer archive added '

---
+++ src/components/Hooks/UseReducer.jsx
@@ -0,0 +1,41 @@
+import { useReducer } from 'react';
+
+const globalState = {
+  title: 'Um titulo aleatorio',
+  body: 'Um bode aleatorio',
+  counter: 0,
+};
+
+// eslint-disable-next-line
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda':
+      console.log('Chamou muda');
+
+      return { ...state, title: 'Mudou' };
+
+    case 'inverter':
+      console.log('Chamou inverter');
+
+      return { ...state, title: 'Um titulo aleatorio' };
+  }
+
+  return { ...state };
+};
+
+export default function UseReducer() {
+  // eslint-disable-next-line
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
+
+  return (
+    <>
+      <h1>
+        {counter} - {title}
+      </h1>
+      {title === 'Um titulo aleatorio' && <button onClick={() => dispatch({ type: 'muda' })}>+</button>}
+      {title === 'Mudou' && <button onClick={() => dispatch({ type: 'inverter' })}>-</button>}
+      <p>{body}</p>
+    </>
+  );
+}
